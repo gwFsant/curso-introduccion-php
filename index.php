80dfc81d4ed7b0a1040a7bbdd5d1190a181f2dbe
Felipe Santiago <f.roldan@greenworkconsultores.com>
2020-07-27 09:13:45 -0500

    Listar registros de la base de datos con Eloquent

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php
 
+
+require_once 'vendor/autoload.php';
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use App\Models\Job;
+
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'cursophp',
+    'username'  => 'root',
+    'password'  => '',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
+
 include_once('jobs.php');
 
 $lastName = 'Roldan Rodriguez';
--- jobs.php
+++ jobs.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'vendor/autoload.php';
+//require_once 'vendor/autoload.php';
 
 //require 'app/Models/Job.php';
 //require 'app/Models/Project.php';
@@ -9,43 +9,47 @@ require_once 'vendor/autoload.php';
 //require_once 'lib1/Project.php';
 
 
-use App\Models\{Job, Project, Printable};
+//use App\Models\{Job, Project, Printable};
+use App\Models\{Job, Project};
 //use lib1\Project;
 //use App\Models\Printable;
 
-$job1 = new Job('PHP Developer','This is an awesome job!!!');
+/*$job1 = new Job('PHP Developer','This is an awesome job!!!');
 $job1->months = 16;
 
 $job2 = new Job('Python Dev','This is an awesome job!!!');
 $job2->months = 5;
 
 $job3 = new Job('Devops','This is an awesome job!!!');
-$job3->months = 24;
+$job3->months = 24;*/
 
-$project1 = new Project('Project 1','Description 1');
+$jobs = Job::all();
+
 
 //$projectlib = new lib1\Project();
 
-$jobs = [
+/*$jobs = [
   $job1,
   $job2,
   $job3   
-  ];
+  ];*/
+
+$project1 = new Project('Project 1','Description 1');
 
 $projects = [
   $project1
 ]; 
   
-  function printElement(Printable $job){
+  function printElement($job){
   
-    if($job->visible == false){
+    /*if($job->visible == false){
       return;
-    }
+    }*/
     
     //Contenido de la funcion
     echo '<li class="work-position">';
-    echo '<h5>' . $job->getTitle() . '</h5>';
-    echo '<p>' . $job->getDescription() . '</p>';
+    echo '<h5>' . $job->title . '</h5>';
+    echo '<p>' . $job->description . '</p>';
     //echo '<p>' . getDuration($job->months) . '</p>';
     echo '<p>' . $job->getDurationAsString() . '</p>';
     echo '<strong>Achievements:</strong>';
